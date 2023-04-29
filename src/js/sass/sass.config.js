8902213bb460763a6ffb6847ac050dd2a3aca058
EVAN <enriquevanegas@outlook.com>
2023-04-29 14:36:02 -0600

    Folder Struct Updated & SASS Installed
    Se actualiza estructura de carpetas y se agrega  SASS al proyecto.

---
+++ src/js/sass/sass.config.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * Archivo para configurar la salida de css
+ * de los archivos SASS.
+ *  
+*/
+
+module.exports = {
+    input: '../css/sass/main.scss',
+    output: '../css/main.css'
+};
